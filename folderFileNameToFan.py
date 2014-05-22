dab2570036f90da5509b1cfae948ae3c7fe5f3b7
Hammer <workthethird@gmail.com>
2014-05-22 12:36:43 +0800

    init commit, at least filename will translate to tc

---
+++ folderFileNameToFan.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from jianfan import jtof
+from subprocess import Popen
+from subprocess import PIPE
+
+global tPath
+
+def prepareRenamePara(oldName,newName):
+	iOut = []
+	iOut.append('mv')
+	iOut.append('-v')
+	iOut.append(oldName)
+	iOut.append(newName)
+	return iOut
+
+def scanAndProduceStack(aPath):
+	outStack = []
+	for dirPath, dirNames, fileNames in os.walk(aPath):
+		outStack.append(dirPath)
+		for f in fileNames:
+			outStack.append(os.path.join(dirPath,f))
+	return outStack
+
+def main():
+	FileNamePathSet = scanAndProduceStack(unicode(tPath))
+	#use pop
+	while len(FileNamePathSet) != 0:
+		oldPath = FileNamePathSet.pop()
+		newPath = jtof(oldPath)
+		#print u"o:(%s) n:(%s)" %(oldPath,newPath)
+		if newPath != oldPath:
+			process = Popen(prepareRenamePara(oldPath,newPath))
+			process.wait()
+
+def verify():
+	argc = len(sys.argv)
+	if argc < 2 or argc > 2 :
+		print "Format : Python %s <foldername>" %(sys.argv[0])
+		exit(1)
+	global tPath
+	tPath = sys.argv[1]
+
+if __name__ == '__main__':
+	verify()
+	main()
---
+++ install.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/bin/sh
+rm -Rvf ~/bin/sh/jianfan
+rm -v ~/bin/sh/gotcfolder
+rm -v ~/bin/sh/folderFileNameToFan.py
+cp -vR jianfan ~/bin/sh
+cp -v gotcfolder ~/bin/sh
+cp -v folderFileNameToFan.py ~/bin/sh
+chmod 755 ~/bin/sh/gotcfolder
